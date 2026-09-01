7ea4428d7b0d736b1c15aa7c84f2b68c17dcfc06
Mark Angelo Tarvina <mttarvina@gmail.com>
2026-09-01 20:17:09 +0800

    Added configurable SPI functionality

---
+++ stm32f303re_dongle/examples/ex04_spi_adaq798x.py
@@ -0,0 +1,131 @@
+import time
+
+import numpy as np
+from signals_toolkit import (
+    ADC_ComputeDynamicRange,
+    ADC_ComputeMetricsRaw,
+    SignalPlotter,
+)
+
+from stm32f303re_dongle import (
+    STM32F303RE_Dongle,
+    scan_stlink,
+)
+from stm32f303re_dongle.dongle import (
+    SPI_ClkFreq,
+    SPI_ClkPhase,
+    SPI_ClkPolarity,
+    SPI_CSNPolarity,
+    SPI_DataWidth,
+    SPI_FirstBit,
+)
+
+
+def main():
+    com_port = scan_stlink()
+    if com_port is None:
+        raise RuntimeError("No STM32/STLINK found!")
+
+    fs = 500_000
+    adc_resolution = 16
+
+    dongle = STM32F303RE_Dongle(port=com_port)
+    dongle.spi_init(
+        data_width=SPI_DataWidth.WIDTH_16,
+        first_bit=SPI_FirstBit.MSB,
+        freq=SPI_ClkFreq.CLK_18MHZ,
+        clk_polarity=SPI_ClkPolarity.IDLE_LOW,
+        clk_phase=SPI_ClkPhase.SECOND_EDGE,
+        sample_rate=fs,
+        transmit_delay=750e-9,
+        csn_pulse_width=750e-9,
+        csn_polarity=SPI_CSNPolarity.ACTIVE_HIGH,
+    )
+
+    num_samples = 1000
+    dongle.spi_transmit(tx_command=[0xFFFF], frame_size=num_samples, increment=False)
+    adc_data = np.array(dongle.buffer_read_rx(frame_size=num_samples))
+    dynamic_range_db = ADC_ComputeDynamicRange(
+        adc_samples=adc_data, adc_resolution=adc_resolution
+    )
+    result = ADC_ComputeMetricsRaw(
+        adc_samples=adc_data, fs=fs, resolution=adc_resolution
+    )
+    print(f"Dynamic Range: {dynamic_range_db} dB")
+
+    plot = SignalPlotter(title="ADC Plots", size=(1920, 1080))
+    plot.add_plot(
+        title="ADC Raw Codes",
+        x_label="Time",
+        x_unit="s",
+        x_data=np.linspace(0, ((1 / fs) * num_samples), num_samples, endpoint=False),
+        y_label="Code",
+        y_unit="",
+        y_data=adc_data,
+        y_range=(0, 2**adc_resolution),
+        pen_color="#FFAA00",
+    )
+    plot.add_plot(
+        title="Spectrum Magnitude (dBFS)",
+        x_label="Frequency",
+        x_unit="Hz",
+        x_data=result["spectrum_freqs"],
+        y_label="Magnitude",
+        y_unit="dBFS",
+        y_data=result["spectrum_mag_dbfs"],
+        y_range=(-150, 0),
+        pen_color="#FFAA00",
+    )
+    plot.add_text(
+        text=f"f0: {result['f0']:.4f} Hz",
+        plot_index=1,
+        pos=(1600, 0),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"f0_mag: {result['f0_mag']:.4f} dBFS",
+        plot_index=1,
+        pos=(1600, 15),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"SNR: {result['snr']:.4f} dB",
+        plot_index=1,
+        pos=(1600, 30),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"SINAD: {result['sinad']:.4f} dB",
+        plot_index=1,
+        pos=(1600, 45),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"THD: {result['thd']:.4f} dB",
+        plot_index=1,
+        pos=(1600, 60),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"ENOB: {result['enob']:.4f} dB",
+        plot_index=1,
+        pos=(1600, 75),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"Peak Signal Level: {result['signal_level_pk']:.4f} dBFS",
+        plot_index=1,
+        pos=(1600, 90),
+        color="#00AAFF",
+    )
+    plot.add_text(
+        text=f"Average Signal Level: {result['signal_level_avg']:.4f} dBFS",
+        plot_index=1,
+        pos=(1600, 105),
+        color="#00AAFF",
+    )
+    plot.show()
+
+
+if __name__ == "__main__":
+    main()
--- stm32f303re_dongle/src/stm32f303re_dongle/dongle.py
+++ stm32f303re_dongle/src/stm32f303re_dongle/dongle.py
@@ -1,3 +1,4 @@
+import struct
 import time
 from dataclasses import dataclass
 from enum import IntEnum, StrEnum
@@ -10,11 +11,14 @@ CMD_ARGUMENT_SIZE = 4
 class CMD_Action(IntEnum):
     CMD_READ = 48
     CMD_WRITE = 49
-    CMD_TOGGLE = 50
-    CMD_START = 51
-    CMD_STOP = 52
-    CMD_CAPTURE = 53
-    CMD_CONFIG = 54
+    CMD_APPEND = 50
+    CMD_TOGGLE = 51
+    CMD_START = 52
+    CMD_STOP = 53
+    CMD_CAPTURE = 54
+    CMD_CONFIG = 55
+    CMD_INIT = 56
+    CMD_RESET = 57
 
 
 class CMD_Subject(IntEnum):
@@ -22,8 +26,10 @@ class CMD_Subject(IntEnum):
     CMD_ADC = 49
     CMD_I2C = 50
     CMD_SPI = 51
-    CMD_TIMER = 52
-    CMD_PWM = 53
+    CMD_SPI_CSN = 52
+    CMD_BUFFER = 53
+    CMD_TIMER = 54
+    CMD_PWM = 55
 
 
 class GPIO_Ports(StrEnum):
@@ -59,6 +65,52 @@ class GPIO_PullModes(StrEnum):
     PULL_DOWN = "2"
 
 
+class SPI_DataWidth(StrEnum):
+    WIDTH_4 = "3"
+    WIDTH_5 = "4"
+    WIDTH_6 = "5"
+    WIDTH_7 = "6"
+    WIDTH_8 = "7"
+    WIDTH_9 = "8"
+    WIDTH_10 = "9"
+    WIDTH_11 = "A"
+    WIDTH_12 = "B"
+    WIDTH_13 = "C"
+    WIDTH_14 = "D"
+    WIDTH_15 = "E"
+    WIDTH_16 = "F"
+
+
+class SPI_FirstBit(StrEnum):
+    MSB = "0"
+    LSB = "1"
+
+
+class SPI_ClkFreq(StrEnum):
+    CLK_18MHZ = "0"
+    CLK_9MHZ = "1"
+    CLK_4P5MHZ = "2"
+    CLK_2P25MHZ = "3"
+    CLK_1P125MHZ = "4"
+    CLK_562P5KHZ = "5"
+    CLK_281P25KHZ = "6"
+
+
+class SPI_ClkPolarity(StrEnum):
+    IDLE_LOW = "0"
+    IDLE_HIGH = "1"
+
+
+class SPI_ClkPhase(StrEnum):
+    FIRST_EDGE = "1"
+    SECOND_EDGE = "2"
+
+
+class SPI_CSNPolarity(StrEnum):
+    ACTIVE_LOW = "0"
+    ACTIVE_HIGH = "1"
+
+
 @dataclass(order=True)
 class CMD_Frame:
     action: CMD_Action
@@ -79,21 +131,21 @@ class CMD_Frame:
 
 
 class STM32F303RE_Dongle:
-    def __init__(self, port, baud_rate: int = 230400, time_out: float = 1.0):
-        self.dev = serial.Serial(port=port, baudrate=baud_rate, timeout=time_out)
+    def __init__(self, port, baud_rate: int = 1500000):
+        self.dev = serial.Serial(port=port, baudrate=baud_rate, timeout=2.0)
 
     def send(self, payload: bytes):
+        print(f"[SENT]:\t{payload}")
         if len(payload) != 14:
             raise RuntimeError("Invalid number of command bytes!")
         self.dev.write(payload)
         self.dev.flush()
+        time.sleep(0.01)
 
     def send_receive(self, payload: bytes):
-        # print(f"[SENT]:\t{payload}")
         self.send(payload=payload)
-        time.sleep(0.01)
         resp = self.dev.read_until(b"\r\n")
-        # print(f"[RESP]:\t{resp}")
+        # print(f"[RESP]:\t{resp.decode('utf-8')}")
         return resp.decode("utf-8")
 
     def gpio_config(
@@ -147,3 +199,148 @@ class STM32F303RE_Dongle:
         except ValueError:
             pin_states = 0
         return pin_states
+
+    def spi_init(
+        self,
+        data_width: SPI_DataWidth,
+        first_bit: SPI_FirstBit,
+        freq: SPI_ClkFreq,
+        clk_polarity: SPI_ClkPolarity,
+        clk_phase: SPI_ClkPhase,
+        sample_rate: int,
+        transmit_delay: float,
+        csn_pulse_width: float,
+        csn_polarity: SPI_CSNPolarity,
+    ):
+        if sample_rate > 800_000:
+            raise ValueError(
+                f"SPI sample rate ({sample_rate}) should not exceed 800kHz!"
+            )
+
+        if transmit_delay > 56.875e-6:
+            raise ValueError(
+                f"SPI transmit delay ({transmit_delay}) should not exceed 56.875us!"
+            )
+
+        if csn_pulse_width < (1 / 72_000_000.0):
+            raise ValueError(
+                f"SPI CSN pulse width ({csn_pulse_width}) should not be less than 13.8889ns!"
+            )
+
+        # configure SPI sample rate
+        sample_rate_khz = int(sample_rate / 1000)
+        cmd = CMD_Frame(
+            CMD_Action.CMD_CONFIG,
+            CMD_Subject.CMD_SPI,
+            "0",
+            f"{sample_rate_khz:04X}",
+            "0000",
+        )
+        self.send(payload=cmd.format())
+
+        # configure SPI CSN pin
+        csn_pulse_width_count = int(csn_pulse_width * 72_000_000.0)
+        cmd = CMD_Frame(
+            CMD_Action.CMD_CONFIG,
+            CMD_Subject.CMD_SPI_CSN,
+            "0",
+            f"{csn_pulse_width_count:04X}",
+            f"000{csn_polarity}",
+        )
+        self.send(payload=cmd.format())
+
+        # configure SPI Module
+        transmit_delay_count = int(transmit_delay * 72_000_000.0)
+        cmd = CMD_Frame(
+            CMD_Action.CMD_CONFIG,
+            CMD_Subject.CMD_SPI,
+            "1",
+            f"{data_width}{first_bit}{freq}{clk_polarity}",
+            f"{clk_phase}{transmit_delay_count:03X}",
+        )
+        self.send(payload=cmd.format())
+        time.sleep(0.01)
+
+        # initialize SPI
+        cmd = CMD_Frame(
+            CMD_Action.CMD_INIT,
+            CMD_Subject.CMD_SPI,
+            "0",
+            "0000",
+            "0000",
+        )
+        self.send(payload=cmd.format())
+
+    def spi_transmit(self, tx_command: list[int], frame_size: int, increment: bool):
+        self.buffer_reset_tx()
+        self.buffer_append_to_tx(data=tx_command)
+        if increment:
+            cmd = CMD_Frame(
+                CMD_Action.CMD_WRITE,
+                CMD_Subject.CMD_SPI,
+                "0",
+                f"{frame_size:04X}",
+                "0001",
+            )
+        else:
+            cmd = CMD_Frame(
+                CMD_Action.CMD_WRITE,
+                CMD_Subject.CMD_SPI,
+                "0",
+                f"{frame_size:04X}",
+                "0000",
+            )
+        self.send(payload=cmd.format())
+
+    def buffer_append_to_tx(self, data: list[int]):
+        for i in data:
+            cmd = CMD_Frame(
+                CMD_Action.CMD_APPEND,
+                CMD_Subject.CMD_BUFFER,
+                "1",
+                f"{i:04X}",
+                "0000",
+            )
+            self.send(payload=cmd.format())
+
+    def buffer_read_rx(self, frame_size: int) -> list[int]:
+        cmd = CMD_Frame(
+            CMD_Action.CMD_READ,
+            CMD_Subject.CMD_BUFFER,
+            "0",
+            f"{frame_size:04X}",
+            "0000",
+        )
+        self.dev.reset_input_buffer()
+        self.send(payload=cmd.format())
+        bytes_to_read = 2 * frame_size
+        data_frame = b""
+        while len(data_frame) < bytes_to_read:
+            buf = self.dev.read(bytes_to_read - len(data_frame))
+            if not buf:
+                raise TimeoutError(
+                    f"Failed to read data! Only captured {len(data_frame)} bytes"
+                )
+            data_frame += buf
+        # print(f"Read bytes: {len(data_frame)}")
+        return list(struct.unpack(f"<{frame_size}H", data_frame))
+
+    def buffer_reset_rx(self):
+        cmd = CMD_Frame(
+            CMD_Action.CMD_RESET,
+            CMD_Subject.CMD_BUFFER,
+            "0",
+            "0000",
+            "0000",
+        )
+        self.send(payload=cmd.format())
+
+    def buffer_reset_tx(self):
+        cmd = CMD_Frame(
+            CMD_Action.CMD_RESET,
+            CMD_Subject.CMD_BUFFER,
+            "1",
+            "0000",
+            "0000",
+        )
+        self.send(payload=cmd.format())
